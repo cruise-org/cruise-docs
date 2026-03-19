11eabb5db0ed8e49999538402d465f8af88a8384
NucleoFusion <lakshit.singh.mail@gmail.com>
2026-03-19 11:23:10 +0530

    feat: initial completion
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,42 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightCatppuccin from '@catppuccin/starlight'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			components: {
+				Footer: './src/components/footer.astro'
+			},
+			lastUpdated: true,
+			plugins: [starlightCatppuccin({
+				dark: { flavor: "mocha", accent: "sapphire" },
+				light: { flavor: "latte", accent: "sapphire" },
+			})],
+			title: 'Cruise',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/cruise-org/cruise' }],
 			sidebar: [
+				{ label: 'Getting Started', link: '/get-started' },
+				{ label: 'Installation', link: '/install' },
 				{
-					label: 'Guides',
+					label: 'Configuration',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Overview', link: '/config' },
+						{ label: 'General', link: '/config/general' },
+						{ label: 'Keybinds', link: '/config/keybinds' },
+						{ label: 'Styles', link: '/config/styles' },
 					],
 				},
+				{ label: 'Contributing', link: '/contributing' },
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Development',
+					items: [
+						{ label: 'Overview', link: '/dev' },
+						{ label: 'Architecture', link: '/dev/architecture' },
+						{ label: 'Interfaces', link: '/dev/interfaces' },
+					],
 				},
 			],
 		}),
